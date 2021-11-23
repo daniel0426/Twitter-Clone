fba59f3fe27fa19453990fd6913c23d6052268f7
daniel0426 <angus9209@gmail.com>
2021-11-23 21:18:42 +1300

    styling finished

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styles from './NotFound.module.css'
+import {AiOutlineArrowRight} from 'react-icons/ai'
 
 const NotFound = () => {
     return (
-        <div>
+        <div className={styles.notFound}>
             <h1>Not found</h1>
+            <Link to='/'><button className={styles.button}>Go Back to Main page <AiOutlineArrowRight className={styles.refreshIcon}/> </button></Link>
         </div>
     )
 }
